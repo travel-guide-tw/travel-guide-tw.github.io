8b3263eb2d103df496db31db67c91fa3709e86bc
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2023-03-16 12:39:53 +0800

    feat(sitemap): create sitemap at build end

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,8 +4,13 @@ import * as dotenv from 'dotenv'
 import gtagHead from './typescript/node/generateSidebar'
 import generateSidebar from './typescript/node/sidebat'
 import generateRewrites from './typescript/node/generateRewrites'
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { SitemapStream } from 'sitemap'
 
 dotenv.config()
+
+const links: { lastmod?: number; url: string }[] = []
 
 export default defineConfig({
   base: '/',
@@ -32,4 +37,21 @@ export default defineConfig({
   },
   title: '台灣軟體人旅遊指南',
   rewrites: generateRewrites(),
+  transformHtml: (_, id, { pageData }) => {
+    if (!/[\\/]404\.html$/.test(id))
+      links.push({
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+        lastmod: pageData.lastUpdated,
+      })
+  },
+  buildEnd: async ({ outDir }) => {
+    const sitemap = new SitemapStream({
+      hostname: 'https://travel-guide-tw.github.io/',
+    })
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+    sitemap.pipe(writeStream)
+    links.forEach((link) => sitemap.write(link))
+    sitemap.end()
+    await new Promise((r) => writeStream.on('finish', r))
+  },
 })
