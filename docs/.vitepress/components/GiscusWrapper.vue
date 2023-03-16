7dad27674d7f6d6b8555c262004fa401e76cf85a
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2023-03-16 22:31:44 +0800

    feat(giscus): init

---
+++ docs/.vitepress/components/GiscusWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper" v-if="shown">
+    <Giscus
+      repo="travel-guide-tw/travel-guide-tw.github.io"
+      repoId="R_kgDOJCizzA"
+      category="General"
+      categoryId="DIC_kwDOJCizzM4CUeRh"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="preferred_color_scheme"
+      lang="zh-TW"
+      loading="lazy"
+      crossorigin="anonymous"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Giscus from '@giscus/vue'
+
+const shown = location.hostname === 'travel-guide-tw.github.io'
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  margin-top: 2rem;
+}
+</style>
--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,9 +1,14 @@
-<script setup>
+<template>
+  <Layout>
+    <template #doc-after>
+      <GiscusWrapper />
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
 import DefaultTheme from 'vitepress/theme'
+import GiscusWrapper from '../components/GiscusWrapper.vue'
 
 const { Layout } = DefaultTheme
 </script>
-
-<template>
-  <Layout></Layout>
-</template>
