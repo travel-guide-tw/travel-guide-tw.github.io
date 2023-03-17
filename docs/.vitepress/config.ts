6964fcbc32310da4c10f86e597f5de3ec387e6dc
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2023-03-18 02:07:42 +0800

    Revert "feat(vitepress): add target: '_self',"

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,7 +38,6 @@ export default defineConfig({
       {
         icon: 'github',
         link: 'https://github.com/travel-guide-tw/travel-guide-tw.github.io/',
-        target: '_self',
       },
     ],
   },
