e7e6b036ef50379aec0dee77d48a53c971c3db5d
ronny1020 <1020ronny@gmail.com>
2024-06-08 17:44:21 +0800

    fix(vitepress): avoid buiding error of undefined on head

--- .vitepress/typescript/node/gtagHead.ts
+++ .vitepress/typescript/node/gtagHead.ts
@@ -3,25 +3,32 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const gtagHead: HeadConfig[] = <string>process.env.VITE_APP_GOOGLE_TAG_ID
-  ? ([
-      [
-        'script',
-        {
-          async: '',
-          src: `https://www.googletagmanager.com/gtag/js?id=${<string>(
-            process.env.VITE_APP_GOOGLE_TAG_ID
-          )}`,
-        },
-      ],
-      [
-        'script',
-        {},
-        `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '${<
-          string
-        >process.env.VITE_APP_GOOGLE_TAG_ID}');`,
-      ],
-    ] as HeadConfig[])
-  : []
+const tagId = <string>process.env.VITE_APP_GOOGLE_TAG_ID
+const tagContent = <string>process.env.VITE_APP_GOOGLE_META_TAG_CONTENT
+
+const gtagHead: HeadConfig[] =
+  tagId && tagContent
+    ? ([
+        [
+          'meta',
+          {
+            name: 'google-site-verification',
+            content: tagContent,
+          },
+        ],
+        [
+          'script',
+          {
+            async: '',
+            src: `https://www.googletagmanager.com/gtag/js?id=${tagId}`,
+          },
+        ],
+        [
+          'script',
+          {},
+          `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '${tagId}');`,
+        ],
+      ] as HeadConfig[])
+    : []
 
 export default gtagHead
