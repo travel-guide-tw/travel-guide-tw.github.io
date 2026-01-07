206dd0d0885006fb23c9ff34a0a9ad7278615509
ronny1020 <1020ronny@gmail.com>
2026-01-07 21:15:18 +0800

    fix: reimplement URL hash generation using the URL object and its properties.

--- .vitepress/utils/getHash.ts
+++ .vitepress/utils/getHash.ts
@@ -1,16 +1,14 @@
 /**
  * Generates a filename-safe string using URL encoding.
- * Removes protocols (http/https), 'www.', and trailing slashes for cleaner names.
- * Manually escapes characters like '*' that are illegal in Windows filenames.
  */
 export default function getHash(url: string): string {
-  const cleanUrl = url
-    .replace(/^https?:\/\//, '')
-    .replace(/^www\./, '')
-    .replace(/\/$/, '')
+  const u = new URL(url)
 
-  return encodeURIComponent(cleanUrl)
-    .replace(/\*/g, '%2A') // Illegal on Windows
-    .replace(/\./g, '%2E') // Avoid extension confusion
-    .substring(0, 250)
+  return [
+    u.hostname,
+    u.pathname.replace(/\/+/g, '/').replace(/[\/\\]/g, '_'),
+    u.search ? u.search.replace(/[^\w]/g, '_') : '',
+  ]
+    .filter(Boolean)
+    .join('')
 }
